62cc1ae6e74dfff5be72fece8fcc2195df3b140c
Xela <alexapilado@live.com>
2018-09-23 23:24:12 -0700

    First commit

---
+++ Assets/Scripts/Menu2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu2 : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(20, Screen.height / 2 + Screen.height / 4, 100, 50), "Menu"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
